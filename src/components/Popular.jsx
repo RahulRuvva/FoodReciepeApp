cd3fddeec775e026e8a2ae48e7cf4077feba04e0
RahulRuvva <90190891+RahulRuvva@users.noreply.github.com>
2023-04-18 14:03:39 +0530

    Added localStorage and stored API data

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -12,12 +12,22 @@ function Popular() {
     useEffect(()=> {
         getPopular();
     }, []);
-    const getPopular = async() => {
+    const getPopular = async () => {
+      const check = localStorage.getItem("popular");
 
+      if(check) 
+      {
+        setPopular(JSON.parse(check));
+      }else 
+      {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=a9db63d77f444e87b9432207309cd0f1&number=9`)
         const data = await api.json();
+        localStorage.setItem("popular", JSON.stringify(data.recipes));
         setPopular(data.recipes);
+        console.log(data.recipes);
+
     };
+  }
   return (
     <div>
       
@@ -34,10 +44,11 @@ function Popular() {
               }}>
               {popular.map((reciepe) => {
                 return(
-                  <SplideSlide>
+                  <SplideSlide key={reciepe.id}>
                     <Card key={reciepe.index}>
                       <p>{reciepe.title}</p>
                       <img src={reciepe.image} alt={reciepe.title}/>
+                      <Gradient/>
                     </Card>
                     </SplideSlide>  
                 );
@@ -58,10 +69,40 @@ const Card = styled.div `
   min-height : 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left:0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
-`
+  p 
+  {
+    position: absolute;
+    z-index : 10;
+    left: 50%;
+    bottom: 0%;
+    transform : translate(-50%, 0%);
+    color: #fff;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1.5rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+const Gradient = styled.div `
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
+`;
 
 export default Popular
