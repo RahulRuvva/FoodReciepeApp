fe3a250250438b702b754665613ac0dfacb57931
RahulRuvva <90190891+RahulRuvva@users.noreply.github.com>
2023-04-18 23:48:13 +0530

    Added Customizable cuisines based on url Params and dynamicall data gets uploaded based upon the url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter as Router } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <h1>Praise be to JESUS</h1>
+      
+    <Router>
+    <h1>Praise be to JESUS</h1>
+      <Category/>
       <Pages/>
+    </Router>  
     </div>
   );
 }
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,16 @@
 import Home from "./Home";
-
+import Cuisine from './Cuisine';
 import React from 'react'
+import {Route, Routes} from 'react-router-dom';
 
 function Pages() {
   return (
     <div>
-        <Home/>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path="/cuisine/:type" element = {<Cuisine />} />
+          </Routes>
+        
     </div>
   )
 }
