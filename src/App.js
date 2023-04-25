84106c67b17fad886df64c004c55a294b98051d0
RahulRuvva <90190891+RahulRuvva@users.noreply.github.com>
2023-04-25 11:26:09 +0530

    Added Logo and Navlinked to Home Page and finished all the individual products linking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
-import { BrowserRouter as Router } from "react-router-dom";
+import {  BrowserRouter as Router } from "react-router-dom";
 import Search from "./components/Search";
+import { Link } from "react-router-dom";
+import { FaCross } from "react-icons/fa";
+import styled from "styled-components";
 
 function App() {
   return (
@@ -9,6 +12,12 @@ function App() {
       
     <Router>
     <h1>Praise be to JESUS</h1>
+    <Nav>
+      <CrossStyle>
+        <FaCross />
+      </CrossStyle>
+      <Logo to ={'/'}>Delicious Recipes</Logo>
+    </Nav>
       <Search/>
       <Category/>
       <Pages/>
@@ -17,4 +26,24 @@ function App() {
   );
 }
 
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster Two', cursive;  
+`
+const Nav = styled.div `
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start; 
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
+
+const CrossStyle = styled.div `
+  color:#DC143C
+  ;
+`
 export default App;
